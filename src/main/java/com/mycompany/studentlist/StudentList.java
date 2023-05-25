f15db9112cfc888e7b82346d3d9e22dd9362cac2
Nayan Mapara <96513736+nayanmapara@users.noreply.github.com>
2023-05-25 18:18:57 -0400

    Update StudentList.java

--- src/main/java/com/mycompany/studentlist/StudentList.java
+++ src/main/java/com/mycompany/studentlist/StudentList.java
@@ -13,5 +13,6 @@ public class StudentList {
     public static void main(String[] args) {
         System.out.println("Hello World!");
         System.out.println("Hello World!");
+        System.out.println("Hello World!");
     }
 }
